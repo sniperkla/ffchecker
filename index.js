cc6eab8488cbe6c90f51b7111726d172f659bfd4
katanyoo angsupanich <katanyoo@sniperkla.local>
2026-06-06 19:15:43 +0700

    test

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const MONGODB_URI =
 const MONGODB_DB = process.env.MONGODB_DB || 'qdragon'
 let mongoClient
 let db
-console.log("hel")
+console.log("heldd")
 
 async function connectMongo() {
   if (!mongoClient) {
